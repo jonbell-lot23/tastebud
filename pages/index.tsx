370a278fa27f1d3005f699d93291892549c7725b
Jon Bell <github@lot23.com>
2023-04-11 21:11:49 +1200

    Fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, Grid, Button } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { useSettingsStore } from '../utilities/store';
 
-const extractRecommendations = (rawResponse) => {
+const extractRecommendation = (rawResponse) => {
   const recommendationStartIndex = rawResponse.indexOf('}') + 1;
   const recommendationText = rawResponse.slice(recommendationStartIndex).trim();
-  const recommendations = recommendationText.split('\n').map((r) => {
-    const [name, reason] = r.split(',', 2).map((s) => s.trim());
-    return { name, reason };
-  });
-  return recommendations;
+  const [name, description] = recommendationText.split(',', 2).map((s) => s.trim());
+  return { name, description };
 };
 
-const fetchRecommendations = async (apiKey, model) => {
+const fetchRecommendation = async (apiKey, model) => {
   const systemMessage =
     'Please recommend four movies to me. They should cover a range of genres and dates, with a bias towards contemporary films. We will be using these movies to help understand your taste so we can make an app around great recommendations. Please return as a JSON in the format of name, reason for recommendation. The reason for recommendation should be concise and focus on why people like it, rather than being a review of the movie.';
   const payload = {
@@ -41,53 +38,44 @@ const fetchRecommendations = async (apiKey, model) => {
 
     const rawResponse = await response.text();
     console.log('API raw response:', rawResponse);
-    const recommendations = extractRecommendations(rawResponse);
+    const recommendation = extractRecommendation(rawResponse);
 
-    return recommendations;
+    return recommendation;
   } catch (error) {
-    console.error('Error fetching recommendations:', error);
-    return [];
+    console.error('Error fetching recommendation:', error);
+    return { name: 'Failed to fetch recommendation', description: '' };
   }
 };
 
 const Index = () => {
-  const [recommendations, setRecommendations] = useState([]);
+  const [recommendation, setRecommendation] = useState({ name: '', description: '' });
   const { apiKey, chatModelId } = useSettingsStore((state) => ({
     apiKey: state.apiKey,
     chatModelId: state.chatModelId,
   }));
 
   useEffect(() => {
-    const fetchAndDisplayRecommendations = async () => {
-      const fetchedRecommendations = await fetchRecommendations(apiKey, chatModelId);
-      setRecommendations(fetchedRecommendations);
+    const fetchAndDisplayRecommendation = async () => {
+      const fetchedRecommendation = await fetchRecommendation(apiKey, chatModelId);
+      setRecommendation(fetchedRecommendation);
     };
 
-    fetchAndDisplayRecommendations();
+    fetchAndDisplayRecommendation();
   }, [apiKey, chatModelId]);
 
-  const handleButtonClick = (movieTitle, action) => {
-    console.log(`You clicked '${action}' for '${movieTitle}'`);
-    // Implement your logic to handle the user's choice
-  };
-
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="sm">
       <Typography variant="h4" component="h1" textAlign="center" mt={5}>
         TasteBuddy
       </Typography>
-      <Grid container spacing={3} mt={5}>
-        {recommendations.length === 0 ? (
-          <Typography variant="body1" component="p" textAlign="center">
-            Fetching recommendations...
-          </Typography>
-        ) : (
-          recommendations.map((movie, index) => (
-            <Grid item xs={12} key={index}>
-              <Typography variant="h6" component="h2">
-                {movie.name}
-              </Typography>
-              <Typography variant="body1" component="p" mb={2}>
-                {movie.reason}
-              </Typography>
-             
+      <Typography variant="h6" component="p" textAlign="center" mt={5}>
+        {recommendation.name ? recommendation.name : 'Fetching recommendation...'}
+      </Typography>
+      <Typography variant="body1" component="p" textAlign="center" mt={1}>
+        {recommendation.description}
+      </Typography>
+    </Container>
+  );
+};
+
+export default Index;
