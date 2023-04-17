3b71ada761310b9db348da73208be555acafc7c6
Jon Bell <github@lot23.com>
2023-04-17 17:05:18 +1200

    Works pretty well

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,9 +72,9 @@ const buildSystemMessage = (preferences) => {
     return message;
   });
 
-  return `Please recommend four movies to me that are not included in the list below. ${preferenceDescriptions.join(
+  return `${preferenceDescriptions.join(
     ' ',
-  )} Considering what movies the person liked, did not like, and are interested in, please recommend four movies that are not mentioned already. Please return your response in a strict JSON object format, with each recommendation containing a name and reason for recommendation. The reason for recommendation should be concise and focus on why people like it, rather than being a review of the movie. Example format: {"1": {"name": "Movie Name", "reason": "Reason"}, "2": {"name": "Movie Name", "reason": "Reason"}}`;
+  )} Considering this list of preferences, please recommend four movies. Please return your response in a strict JSON object format, with each recommendation containing a name and reason for recommendation. The reason for recommendation should be concise and focus on why people like it, rather than being a review of the movie. Example format: {"1": {"name": "Movie Name", "reason": "Reason"}, "2": {"name": "Movie Name", "reason": "Reason"}} Remember, do not recommend anything that has already been listed in the list I provided earlier. Respond as if you're a friendly and helpful Blockbuster clerk who knows me well. Sometimes try more controversial movies I might not have heard of so I can tune the algorithm. DO NOT RETURN ANY MOVIES I ALREADY MENTIONED.`;
 };
 
 const fetchMoviePreferences = async () => {
